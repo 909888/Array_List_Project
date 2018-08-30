1deda349a8515c1b751753db927f2c88268f9d9b
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-08-29 20:08:32 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_And_List_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //EX 1:
+            //int userInput;
+
+            //Console.WriteLine("Hello, human. Please select a number from 0 to 7");
+            //userInput = Convert.ToInt32(Console.ReadLine());
+
+            //string[] numArray = new string[] { "Hello", "Hey", "Hola", "Bonjour", "Guten tag", "What do you want?", "Hi" };
+
+
+            //for (int i = 0; i < numArray.Length; i++)
+            //{
+            //    if (i == userInput)
+            //    {
+            //        Console.WriteLine("Value of i: {0}", numArray[i]);
+            //    }
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+            //}
+
+            //Console.WriteLine(newArray == numArray[]);
+            //Console.ReadLine();
+
+            //Ex 2: 
+            //int userInput;
+
+            //Console.WriteLine("Hello, human. Please select a number from 0 to 7");
+            //userInput = Convert.ToInt32(Console.ReadLine());
+
+
+            //int[] numArray = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+
+
+
+            //for (int i = 0; i < numArray.Length; i++)
+            //{
+            //    if (i == userInput)
+            //    {
+            //        Console.WriteLine("Value of i: {0}", numArray[i]);
+            //    }
+
+            // }
+
+
+            //Console.ReadLine();
+
+
+            //EX 3:
+            //int userInput;
+
+            //Console.WriteLine("Hello, human. Please select a number from 0 to 7");
+            //userInput = Convert.ToInt32(Console.ReadLine());
+
+            //int[] numArray = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+
+
+            //for (int i = 0; i < numArray.Length; i++)
+            //{
+            //    if (i == userInput)
+            //    {
+            //        Console.WriteLine("Value of i: {0}", numArray[i]);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("no");
+            //    }
+
+            //}
+            //    Console.ReadLine();
+
+            //EX 4:
+            //int userInput; 
+
+            //Console.WriteLine("Hello, human. Please select a number from 0 to 7");
+            //userInput = Convert.ToInt32(Console.ReadLine());
+
+            //List<string> intList = new List<string>();
+            //intList.Add("Hello");
+            //intList.Add("ed");
+            //intList.Add("Kyle");
+            //intList.Add("Kenny");
+            //intList.Add("Stan");
+            //intList.Add("Eric");
+            //intList.Add("Butters");
+
+            //for (int i = 0; i < intList.Count; i++)
+            //{
+            //    if (i == userInput)
+            //    {
+            //        Console.WriteLine("Value of i: {0}", intList[i]);
+            //    }
+
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
